a0a1f6803f09a59ace30a7fe333c2a8af34f38e8
dimsumham <130201256+dimsumham@users.noreply.github.com>
2023-08-31 22:28:47 -0600

    still not working, but getting closer.

--- popup.js
+++ popup.js
@@ -34,38 +34,51 @@ document.getElementById('companyName').addEventListener('keypress', function (e)
 });
 
 
+// What do I need to do on click? 
+// Go to targetUrl.
+// Check if we've arrived at targetUrl. If we did, continue. If not, load targetUrl.
+// Sometimes, you're brought to https://www.sedarplus.ca/landingpage/
+// Clear local storage.
+// send message: 'search' and companyName
+// send 'reset count'
+
+
 function performSearch() {
+  // clear statusPane
   let statusPane = document.getElementById('statusPane');
-  if (statusPane) {
-    statusPane.innerHTML = '';
-  }
-  chrome.storage.loca.clear(function() {
+  statusPane.innerHTML = '';
+  console.log('Search Request Received.')
+  // clear chrome storage
+  chrome.storage.local.clear(function() {
     console.log("Storage Cleared.")
   });
-  const companyName = document.getElementById('companyName').value;
-  console.log('Search Request Received.')
   console.log('Navigating to Sedar+.')
+  
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const tabId = tabs[0].id;
     const targetUrl = 'https://www.sedarplus.ca/csa-party/service/create.html?targetAppCode=csa-party&service=searchReportingIssuers&_locale=en'
     const landingUrl = 'https://www.sedarplus.ca/landingpage/'
 
-    chrome.tabs.update(tabId, { url: targetUrl });
-    setTimeout(() => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.update(tabId, { url: targetUrl }, function(tab) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         if (tabs[0].url === landingUrl) {
           console.log("Sedar+ Rerouted us. Reloading.");
-          chrome.tabs.update(tabId, { url: targetUrl });
+          chrome.tabs.update(tabId, { url: targetUrl }, function(tab) {
+            sendMessage(tabId);
+          });
         } else {
           console.log("Correct site");
+          sendMessage(tabId);
         }
       });
-    }, 500);
-    
-    let selectedValues = $('#documentType').multiselect('getSelectedValues');
-    chrome.storage.local.set({ searchRequested: true, companyName: companyName, selectedValues: selectedValues }); 
-    console.log('Sending Message')
-    chrome.tabs.sendMessage(tabId, { action: 'search', companyName});
-    chrome.runtime.sendMessage({ action: 'reset_count' });
-  });
+    });
+  })};
+
+function sendMessage(tabId) {
+  console.log('Sending Message')
+  const companyName = document.getElementById('companyName').value;    
+  let selectedValues = $('#documentType').multiselect('getSelectedValues');
+  chrome.storage.local.set({ searchRequested: true, companyName: companyName, selectedValues: selectedValues }); 
+  chrome.tabs.sendMessage(tabId, { action: 'search', companyName: companyName});
+  chrome.runtime.sendMessage({ action: 'reset_count' });
 }
